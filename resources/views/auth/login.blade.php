9f0ba3434e4d8c849ed92bd1c583b5713f559530
Ahmad Eyamin <ahmadeyamin@gmail.com>
2020-06-22 19:10:56 +0600

    Basic Auth Page Designed & Added Theme Assets

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,95 @@
-@extends('layouts.app')
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Login | Laravel-Starter - Admin Template</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+        <link rel="icon" href="../favicon.ico" type="image/x-icon" />
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        <link rel="stylesheet" href="{{ asset('plugins/bootstrap/dist/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('plugins/ionicons/dist/css/ionicons.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('plugins/icon-kit/dist/css/iconkit.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('dist/css/theme.min.css') }}">
+        <script src="{{ asset('src/js/vendor/modernizr-2.8.3.min.js') }}"></script>
+    </head>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+    <body>
+        <div class="auth-wrapper">
+            <div class="container-fluid h-100">
+                <div class="row flex-row h-100 bg-white">
+                    <div class="col-xl-8 col-lg-6 col-md-5 p-0 d-md-block d-lg-block d-sm-none d-none">
+                    <div class="lavalite-bg" style="background-image: url('{{asset('img/auth/login-bg.jpg')}}')">
+                            <div class="lavalite-overlay"></div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-7 my-auto p-0">
+                        <div class="authentication-form mx-auto">
+                            <div class="logo-centered">
+                                <a href="/"><img src="{{ asset('src/img/brand.svg') }}" alt=""></a>
                             </div>
-                        </div>
+                            <h3>Sign In to Laravel-Starter</h3>
+                            <p>Happy to see you again!</p>
+                            <form action="{{ route('login') }}" method="POST">
+                                <div class="form-group">
+                                    <input type="text" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email" required="" />
+                                    <i class="ik ik-user"></i>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    @error('email')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
                                 </div>
-                            </div>
-                        </div>
+                                @csrf
+                                <div class="form-group">
+                                    <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" required="" name="password" />
+                                    <i class="ik ik-lock"></i>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                                    @error('password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                                <div class="row">
+                                    <div class="col text-left">
+                                        <label class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="item_checkbox" {{ old('remember') ? 'checked' : '' }} name="item_checkbox" value="option1">
+                                            <span class="custom-control-label"> {{ __('Remember Me') }}</span>
+                                        </label>
+                                    </div>
+                                    <div class="col text-right">
+                                    <a href="#">{{__('Forgot Password ?')}}</a>
+                                    </div>
+                                </div>
+                                <div class="sign-btn text-center">
+                                <button class="btn btn-theme">{{__('Sign In')}}</button>
+                                </div>
+                            </form>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                            @if (Route::has('register'))
+                            <div class="register">
+                                <p>Don't have an account? <a href="{{ route('register')}}">Create an account</a></p>
                             </div>
+                            @endif
+
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="{{ asset('plugins/popper.js/dist/umd/popper.min.js') }}"></script>
+        <script src="{{ asset('plugins/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+    </body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,111 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Login | Laravel-Starter - Admin Template</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+        <link rel="icon" href="../favicon.ico" type="image/x-icon" />
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+        <link rel="stylesheet" href="{{ asset('plugins/bootstrap/dist/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('plugins/ionicons/dist/css/ionicons.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('plugins/icon-kit/dist/css/iconkit.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('dist/css/theme.min.css') }}">
+        <script src="{{ asset('src/js/vendor/modernizr-2.8.3.min.js') }}"></script>
+    </head>
+
+    <body>
+        <div class="auth-wrapper">
+            <div class="container-fluid h-100">
+                <div class="row flex-row h-100 bg-white">
+                    <div class="col-xl-8 col-lg-6 col-md-5 p-0 d-md-block d-lg-block d-sm-none d-none">
+                    <div class="lavalite-bg" style="background-image: url('{{asset('img/auth/login-bg.jpg')}}')">
+                            <div class="lavalite-overlay"></div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-6 col-md-7 my-auto p-0">
+                        <div class="authentication-form mx-auto">
+                            <div class="logo-centered">
+                                <a href="{{ route('home') }}"><img src="{{ asset('src/img/brand.svg') }}" alt="logo"></a>
+                            </div>
+                            <h3>New to Laravel-Starter</h3>
+                            <p>Join us today! It takes only few steps</p>
+                            <form action="{{ route('register') }}" method="POST">
+
+                                <div class="form-group">
+                                    <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" placeholder="Name" required="">
+                                    <i class="ik ik-user"></i>
+
+                                    @error('name')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+
+
+                                <div class="form-group">
+                                    <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="Email" required="">
+                                    <i class="ik ik-mail"></i>
+
+                                    @error('email')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control @error('password') is-invalid  @enderror" name="password" placeholder="Password" required="">
+                                    <i class="ik ik-lock"></i>
+
+                                    @error('password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                                @csrf
+                                <div class="form-group">
+                                    <input type="password" name="password_confirmation" class="form-control @error('password_confirmation') is-invalid @enderror"  placeholder="Confirm Password" required="">
+                                    <i class="ik ik-eye-off"></i>
+
+                                    @error('password_confirmation')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                    @enderror
+                                </div>
+                                <div class="row">
+                                    <div class="col-12 text-left">
+                                        <label class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="item_checkbox" name="item_checkbox" required value="1">
+                                            <span class="custom-control-label">&nbsp;I Accept <a href="#">Terms and Conditions</a></span>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="sign-btn text-center">
+                                <button type="submit" class="btn btn-theme">{{__('Create Account')}}</button>
+                                </div>
+                            </form>
+                            <div class="register">
+                                <p>Already have an account? <a href="{{ route('login') }}">Sign In</a></p>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script src="{{ asset('plugins/popper.js/dist/umd/popper.min.js') }}"></script>
+        <script src="{{ asset('plugins/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+    </body>
+</html>
