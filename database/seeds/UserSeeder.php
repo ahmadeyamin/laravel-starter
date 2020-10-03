8d6c0b20a0df72791d0218ce126d0cc30edc5bc4
Ahmad Eyamin <ahmadeyamin@gmail.com>
2020-10-04 04:15:55 +0600

    Update UserSeeder.php

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -18,6 +18,9 @@ class UserSeeder extends Seeder
             'name' => 'Admin',
             'email' => 'admin@admin.com',
             'status' => true,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+
             'last_login_ip' => null,
             'provider_token' => null,
             'provider' => null,
