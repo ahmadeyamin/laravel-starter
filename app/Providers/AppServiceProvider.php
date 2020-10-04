c17978bd3ea890e891c792011cf28fba702b0c8f
Ahmad Eyamin <ahmadeyamin@gmail.com>
2020-10-04 22:52:04 +0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\Module;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\Setting;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +29,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Blade::withoutDoubleEncoding();
+
+
+        if(env('REDIRECT_HTTPS'))
+        {
+            \URL::forceScheme('https');
+        }
+
+        Blade::directive('datetime', function ($date) {
+            return "<?php echo ($date)->format('m/d/Y h:iA'); ?>";
+        });
+
+        Paginator::useBootstrap();
+
+        Module::observe(\App\Observers\ModuleObserver::class);
+        Permission::observe(\App\Observers\PermissionObserver::class);
+        Role::observe(\App\Observers\RoleObserver::class);
+        Setting::observe(\App\Observers\SettingObserver::class);
     }
 }
